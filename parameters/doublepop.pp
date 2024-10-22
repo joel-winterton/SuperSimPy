4388200d8a1fd7c26fd2d554f855d44458306cb9
Joel <joel.winterton@outlook.com>
2024-10-22 15:53:15 +0100

    Two site haplotype mutations

--- parameters/doublepop.pp
+++ parameters/doublepop.pp
@@ -1,4 +1,4 @@
 #Population_format_version 0.0.1
 id size contactDensity "conDenAfterLD startLD endLD" samplingMultiplier
-0 500000000 1.0 0.1,5,1 0.2
-1 500000000 1.0 0.1,5,1 0.2
+0 500000000 1.0 0.1,1,1 0.2
+1 500000000 1.0 0.1,1,1 0.2
